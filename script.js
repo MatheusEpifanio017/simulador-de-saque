679bf8865cf194695ede7a64773a61fc0c4fc615
Matheus Souza <mthsepifanio@gmail.com>
2026-03-18 21:08:53 -0300

    Adição do sistema de deposito

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let saldo = 200;
 
 const $inputSaque = document.getElementById("ValorDoSaque");
+
 const $displaySaldo = document.getElementById("saldo");
 
+const $inputDeposito = document.getElementById("ValorDoDeposito")
+
 const $displayMensagem = document.getElementById("mensagem");
+ 
+const $displayMensagem2 = document.getElementById("mensagem2")
 
 $displaySaldo.innerText = saldo;
 
@@ -12,24 +17,48 @@ function realizarSaque() {
     const valorASacar = (Number($inputSaque.value));
 
     if (isNaN(valorASacar) || valorASacar < 0) {
-        $displayMensagem.innerText = "valor digitado é invalido"
+        $displayMensagem.innerText = "O VALOR DIGITADO É INVALIDO"
         return
     }
 
 
     if (valorASacar > saldo) {
-        $displayMensagem.innerText = "Não tem saldo"
+        $displayMensagem.innerText = "NÃO POSSUI SALDO SUFIENTE!"
         return 
     }
    
 
 
-     saldo = saldo - valorASacar.toFixed(2);
+     saldo = saldo - valorASacar
      $displaySaldo.innerText = saldo;
 
-     $displayMensagem.innerText = "saque realizado"
+     $displayMensagem.innerText = "SAQUE REALIZADO!"
 
+  
+}
 
+function realizarDeposito() {
+
+    const ValorDoDeposito = (Number($inputDeposito.value));
+     
+    if (ValorDoDeposito < 1) {
+        $displayMensagem2.innerText = "VALOR NÃO SUFICIENTE PARA DEPOSITO!"
+        return
+    }
+     
+ 
+        
+    
+ console.log(saldo,ValorDoDeposito)
+
+    saldo = saldo + ValorDoDeposito
+
+    console.log(saldo)
+
+    $displaySaldo.innerText = saldo
+    $displayMensagem2.innerText = "DEPOSITO REALIZADO!"
     
 }
+    
+
 
